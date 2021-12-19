2c37a890da8d9d6d7ec2dc7957b72d8ee0b0d4e4
NIR <MAILMAIL10@GMAIL.COM>
2021-12-19 14:30:15 +0200

    code optimization

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar/Navbar.js';
 import Card from '../Card/Card.js';
 
@@ -23,6 +23,22 @@ const Movies = () => {
             })
     }
 
+    useEffect(() => {
+        let URL = `http://localhost:3001/api/super`
+
+        fetch(URL, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then((result) => {
+                setMovies(result.data.Search)
+
+            })
+    }, [])
+
     return (
         <div>
             <Navbar
@@ -32,7 +48,7 @@ const Movies = () => {
             />
 
             <div className="cardlistMovies">
-                {movies ? movies.filter(movie => movie.Poster).map((movie) => <Card key={movie.imdbID} movie={movie} />)
+                {movies ? movies.filter((movie) => movie.Poster).map((movie) => <Card key={movie.imdbID} movie={movie} />)
                     :
                     <h3>Movie Not Found, Try another movie</h3>}
             </div>
--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 require('dotenv').config();
 const fetch = require('node-fetch');
-const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
